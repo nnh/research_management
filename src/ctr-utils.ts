44d5dde4ab495306cc22148fc9590b3cee2b16f5
Katsusuke Shimizu <katsusuke@gmail.com>
2019-07-08 17:10:41 +0900

    refactoring

--- src/ctr-utils.ts
+++ src/ctr-utils.ts
@@ -1,12 +1,16 @@
+export function isUminId(id: string): boolean{
+  return /(UMIN|C)\d{9}/.test(id)
+}
+
 export function getUminIds(targetSheetValues: any[][], column: number): any[] {
   // targetSheet内に記載されているUMINIDを取得する
-  var uminIds = [];
+  const uminIds = []
 
   for (var i = 1; i < targetSheetValues.length; i++) {
+
     var ids = targetSheetValues[i][column].split(',');
     for (var j = 0; j < ids.length; j++) {
-      if (/(UMIN|C)\d{9}/.test(ids[j])) { uminIds.push(ids[j]); }
-      // if (ids[j].indexOf('UMIN') != -1) { uminIds.push(ids[j]); }
+      if (isUminId(ids[j])) { uminIds.push(ids[j]); }
     }
   }
 
