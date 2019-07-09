069d3d1f0da9bc523919b2ef7eead647c34f2dd8
Katsusuke Shimizu <katsusuke@gmail.com>
2019-07-09 13:04:17 +0900

    refactoring

--- src/xml.ts
+++ src/xml.ts
@@ -1,12 +1,7 @@
 import XML_Service = GoogleAppsScript.XML_Service
 
 export function getElementsByTagName(element: XML_Service.Element, tagName: string) {
-  var data = [], descendants = element.getDescendants();
-  for(var i = 0; i < descendants.length; i++) {
-    var elem = descendants[i].asElement();
-    if ( elem != null && elem.getName() == tagName) data.push(elem);
-  }
-  return data;
+  return filterElements(element, (elem) => elem.getName() == tagName)
 }
 
 export function getElementValue(target: XML_Service.Element, name: string) {
