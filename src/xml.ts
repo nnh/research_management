4741479edb0334380401ff5ff48bb95c8938bedf
Katsusuke Shimizu <katsusuke@gmail.com>
2019-07-09 13:01:51 +0900

    add general function

--- src/xml.ts
+++ src/xml.ts
@@ -13,3 +13,15 @@ export function getElementValue(target: XML_Service.Element, name: string) {
   var element = getElementsByTagName(target, name);
   return element.length ? element[0].getValue() : '';
 }
+
+export function filterElements(element: XML_Service.Element, predicate: (elm: XML_Service.Element) => any): XML_Service.Element[] {
+  const res = predicate(element) ? [element] : []
+  const descendants = element.getDescendants()
+  for (let i = 0; i < descendants.length; ++i) {
+    if (descendants[i].getType() === XML_Service.ContentType.ELEMENT) {
+      const e = descendants[i].asElement()
+      if (predicate(e)) res.push(e)
+    } 
+  }
+  return res
+}
