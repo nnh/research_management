cf3a09aab67a7118092007ade7999cec8d5a244d
Katsusuke Shimizu <katsusuke@gmail.com>
2019-07-09 16:35:31 +0900

    add test

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,5 +1,5 @@
 import { assert } from "chai"
-import { readValues } from "../src/utils.ts"
+import { readValues, arrayUniq } from "../src/utils.ts"
 
 describe("readValues", () => {
   context('with normal table', () => {
@@ -13,6 +13,15 @@ describe("readValues", () => {
       const array = readValues(table)
       assert.equal(array[0].name, 'apple')
       assert.equal(array[2].count, 2)
+    })
+  })
+})
+
+describe("arrayUniq", () => {
+  context('with duplicated array', () => {
+    it("changes uniquely", () => {
+      const array = arrayUniq([1, 2, 1, 3, 3, 4])
+      assert.sameMembers(array, [1, 2, 3, 4])
     })
   })
 })
