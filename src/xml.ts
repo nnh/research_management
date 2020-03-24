7690ddae245a0aea1509a206b36fb19855ea340c
Katsusuke Shimizu <katsusuke@gmail.com>
2020-03-24 14:56:04 +0900

    fix type

--- src/xml.ts
+++ src/xml.ts
@@ -17,19 +17,19 @@ export function getElementValue(target: XML_Service.Element, name: string) {
   return element.length ? element[0].getValue() : '';
 }
 
-export function filterElements(element: XML_Service.Element, predicate: (elm: XML_Service.Element, index: number, descendents: XML_Service.Content[]) => any): XML_Service.Element[] {
+export function filterElements(element: XML_Service.Element, predicate: (elm: XML_Service.Element, index: number, descendents: XML_Service.Content[]) => boolean): XML_Service.Element[] {
   const res = []
   const descendants = element.getDescendants()
   for (let i = 0; i < descendants.length; ++i) {
     if (descendants[i].getType() === XmlService.ContentTypes.ELEMENT) {
       const e = descendants[i].asElement()
       if (predicate(e, i, descendants)) res.push(e)
-    } 
+    }
   }
   return res
 }
 
-export function findElement(element: XML_Service.Element, predicate: (elm: XML_Service.Element, index: number, descendents: XML_Service.Content[]) => any): XML_Service.Element|undefined {
+export function findElement(element: XML_Service.Element, predicate: (elm: XML_Service.Element, index: number, descendents: XML_Service.Content[]) => boolean): XML_Service.Element|undefined {
   const descendants = element.getDescendants()
   for (let i = 0; i < descendants.length; ++i) {
     if (descendants[i].getType() === XmlService.ContentTypes.ELEMENT) {
