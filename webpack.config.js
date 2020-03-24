c045a4cf8e38e634fbc202a4c023c37e4234b5de
Katsusuke Shimizu <katsusuke@gmail.com>
2020-03-24 17:59:58 +0900

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const path = require('path')
+
+const entry = fs.readdirSync("src").filter(s => s.match(/\.ts$/)).reduce((obj, str) => ({...obj, ...{[path.basename(str, '.ts')]: './src/' + str}}), {})
+console.log({entry})
+
+module.exports = {
+  mode: "development",
+  entry,
+  output: {
+    filename: '[name].js',
+    path: __dirname + '/dist'
+  },
+  devtool: 'inline-source-map',
+  resolve: {
+    modules: [
+      "node_modules",
+    ],
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "ts-loader" },
+    ],
+  },
+  plugins: [],
+};
