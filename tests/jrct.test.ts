89eb20a6db08f64ddb8bfcf7dc7a448f59795014
Katsusuke Shimizu <katsusuke@gmail.com>
2020-04-03 12:16:21 +0900

    fix

--- tests/jrct.test.ts
+++ tests/jrct.test.ts
@@ -10,12 +10,14 @@ describe('getDescriptionByHtml', () => {
       const html = fs.readFileSync(`tests/fixtures/jrct_${jRctId}.html`).toString()
       const data = getDescriptionByHtml(html)
       assert.equal(data.condition, '急性白血病')
-      assert.equal(data.interventions,
-                   "FLAMEL regimen:リン酸フルダラビン(30 mg/square meter×1回/日を4日間)と、シタラビン（2 g/square meter×1回/日を2日間）、メルファラン（60 mg/square を3日間）、全身照射（3 Gy 1ｘ1/日）\n" +
-                   "移植片は骨髄、末梢血幹細胞、臍帯血のいずれも選択できる。原則的に，タクロリムスまたはシクロスポリンとメソトレキサートを用いたGVHD予防を行う。\n" +
-                   "副次的項目の評価のために、コントロールとして骨髄髄破壊的量のブスルファン（> 8 mg/kg）または全身照射（>= 8 Gy, fractionated）を用いた前処置後の造血細胞移植を受けた症例が登録される。\n" +
-                   "移植片は骨髄、末梢血幹細胞、臍帯血のいずれも選択できる。原則的に，タクロリムスまたはシクロスポリンとメソトレキサートを用いたGVHD予防を行う。"
-                  )
+
+      const expected =
+        "FLAMEL regimen:リン酸フルダラビン(30 mg/square meter×1回/日を4日間)と、シタラビン（2 g/square meter×1回/日を2日間）、メルファラン（60 mg/square を3日間）、全身照射（3 Gy 1ｘ1/日） \r\n" +
+        "移植片は骨髄、末梢血幹細胞、臍帯血のいずれも選択できる。原則的に，タクロリムスまたはシクロスポリンとメソトレキサートを用いたGVHD予防を行う。\r\n" +
+        "副次的項目の評価のために、コントロールとして骨髄髄破壊的量のブスルファン（> 8 mg/kg）または全身照射（>= 8 Gy, fractionated）を用いた前処置後の造血細胞移植を受けた症例が登録される。 \r\n" +
+        "移植片は骨髄、末梢血幹細胞、臍帯血のいずれも選択できる。原則的に，タクロリムスまたはシクロスポリンとメソトレキサートを用いたGVHD予防を行う。"
+
+      assert.equal(data.interventions, expected)
     })
   })
 })
