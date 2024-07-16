0c82ea5effe6a505bff917db4c22ddde970b85b8
MarikoOhtsuka <mariko.ohtsuka@nnh.go.jp>
2024-07-16 16:09:24 +0900

    mod test youshiki

--- src/test-youshiki.ts
+++ src/test-youshiki.ts
@@ -47,7 +47,20 @@ export class TestYoushiki extends testCommon.TestScript {
       const tempArray = new Array(inputValues[utils.headerRowIndex].length);
       for (let col = 0; col < inputValues[utils.headerRowIndex].length; col++) {
         if (String(inputValues[row][col]) !== String(compareValues[row][col])) {
-          tempArray[col] = "NG";
+          const tempValue: unknown = inputValues[row][col]; // まず unknown に変換
+          const tempType: string = Object.prototype.toString.call(tempValue);
+          if (tempType == "[object Date]") {
+            const dateValue: Date = tempValue as Date; // Date 型への安全なキャスト
+            const month: string = String(dateValue.getMonth() + 1);
+            const day: string = String(dateValue.getDate());
+            if (`${month}-${day}` === String(compareValues[row][col])) {
+              tempArray[col] = "OK";
+            } else {
+              tempArray[col] = "NG";
+            }
+          } else {
+            tempArray[col] = "NG";
+          }
         } else {
           tempArray[col] = "OK";
         }
@@ -64,84 +77,64 @@ export class TestYoushiki extends testCommon.TestScript {
     targetRange.setValues(outputValues);
     this.setConditionalFormatting(outputSheet, targetRange);
   }
-  execTestYoushiki2_1(): void {
-    const outputYoushikiSheet: GoogleAppsScript.Spreadsheet.Sheet =
-      this.getCheckSheet_("checkYoushiki2_1");
-    const outputBettenSheet1: GoogleAppsScript.Spreadsheet.Sheet =
-      this.getCheckSheet_("checkBetten2_1_1");
-    const outputBettenSheet2: GoogleAppsScript.Spreadsheet.Sheet =
-      this.getCheckSheet_("checkBetten2_1_2");
-    const compareYoushikiSheet: GoogleAppsScript.Spreadsheet.Sheet =
-      this.getWkSheetByName_(this.testSs, "youshiki2_1");
-    const compareBettenSheet1: GoogleAppsScript.Spreadsheet.Sheet =
-      this.getWkSheetByName_(this.testSs, "betten2_1_1");
-    const compareBettenSheet2: GoogleAppsScript.Spreadsheet.Sheet =
-      this.getWkSheetByName_(this.testSs, "betten2_1_2");
+  private testYoushiki_(
+    outputSheetName: string,
+    compareSheetName: string,
+    inputSheetName: string
+  ): void {
+    const outputSheet: GoogleAppsScript.Spreadsheet.Sheet =
+      this.getCheckSheet_(outputSheetName);
+    const compareSheet: GoogleAppsScript.Spreadsheet.Sheet =
+      this.getWkSheetByName_(this.testSs, compareSheetName);
     const inputYoushikiSheet: GoogleAppsScript.Spreadsheet.Sheet =
-      this.getWkSheetByName_(
-        this.youshikiSs,
-        utils.outputYoushiki2SheetNames.get("youshiki2_1_2")!
-      );
-    const inputBettenSheet1: GoogleAppsScript.Spreadsheet.Sheet =
-      this.getWkSheetByName_(
-        this.youshikiSs,
-        utils.outputYoushiki2SheetNames.get("attachment2_1_1")!
-      );
-    const inputBettenSheet2: GoogleAppsScript.Spreadsheet.Sheet =
-      this.getWkSheetByName_(
-        this.youshikiSs,
-        utils.outputYoushiki2SheetNames.get("attachment2_1_2")!
-      );
-    this.compareValues(
-      inputYoushikiSheet,
-      compareYoushikiSheet,
-      outputYoushikiSheet
+      this.getWkSheetByName_(this.youshikiSs, inputSheetName);
+    this.compareValues(inputYoushikiSheet, compareSheet, outputSheet);
+  }
+  execTestYoushiki2_1(): void {
+    this.testYoushiki_(
+      "checkYoushiki2_1",
+      "youshiki2_1",
+      utils.outputYoushiki2SheetNames.get("youshiki2_1_2")!
     );
-    this.compareValues(
-      inputBettenSheet1,
-      compareBettenSheet1,
-      outputBettenSheet1
+    this.testYoushiki_(
+      "checkBetten2_1_1",
+      "betten2_1_1",
+      utils.outputYoushiki2SheetNames.get("attachment2_1_1")!
     );
-    this.compareValues(
-      inputBettenSheet2,
-      compareBettenSheet2,
-      outputBettenSheet2
+    this.testYoushiki_(
+      "checkBetten2_1_2",
+      "betten2_1_2",
+      utils.outputYoushiki2SheetNames.get("attachment2_1_2")!
     );
   }
-  execTestYoushiki2_2(): void {}
-  execTestYoushiki3(): void {
-    const outputYoushikiSheet: GoogleAppsScript.Spreadsheet.Sheet =
-      this.getCheckSheet_("checkYoushiki3");
-    const outputBettenSheet1: GoogleAppsScript.Spreadsheet.Sheet =
-      this.getCheckSheet_("checkBetten3");
-    const compareYoushikiSheet: GoogleAppsScript.Spreadsheet.Sheet =
-      this.getWkSheetByName_(this.testSs, "youshiki3");
-    const compareBettenSheet1: GoogleAppsScript.Spreadsheet.Sheet =
-      this.getWkSheetByName_(this.testSs, "betten3");
-    const inputYoushikiSheet: GoogleAppsScript.Spreadsheet.Sheet =
-      this.getWkSheetByName_(
-        this.youshikiSs,
-        utils.outputYoushiki3SheetNames.get("youshiki3_1")!
-      );
-    const inputBettenSheet1: GoogleAppsScript.Spreadsheet.Sheet =
-      this.getWkSheetByName_(
-        this.youshikiSs,
-        utils.outputYoushiki3SheetNames.get("attachment3")!
-      );
-    this.compareValues(
-      inputYoushikiSheet,
-      compareYoushikiSheet,
-      outputYoushikiSheet
+  execTestYoushiki2_2(): void {
+    this.testYoushiki_(
+      "checkYoushiki2_2",
+      "youshiki2_2",
+      utils.outputYoushiki2SheetNames.get("youshiki2_2_2")!
     );
-    this.compareValues(
-      inputBettenSheet1,
-      compareBettenSheet1,
-      outputBettenSheet1
+    this.testYoushiki_(
+      "checkBetten2_2",
+      "betten2_2",
+      utils.outputYoushiki2SheetNames.get("attachment2_2")!
+    );
+  }
+
+  execTestYoushiki3(): void {
+    this.testYoushiki_(
+      "checkYoushiki3",
+      "youshiki3",
+      utils.outputYoushiki3SheetNames.get("youshiki3_1")!
+    );
+    this.testYoushiki_(
+      "checkBetten3",
+      "betten3",
+      utils.outputYoushiki3SheetNames.get("attachment3")!
     );
   }
   execTest() {
     this.execTestYoushiki3();
-    return;
+    this.execTestYoushiki2_2();
     this.execTestYoushiki2_1();
   }
 }
